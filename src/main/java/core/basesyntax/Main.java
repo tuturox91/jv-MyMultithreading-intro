85beeba78ff25dad8016b7678f4b0adf3ff8730a
Igor Bukur <tuturox91@gmail.com>
2022-12-26 14:59:28 +0200

    Finish

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread counterThread = new MyThread();
+
+        Runnable runnableThread = new MyRunnable();
+        Thread infiniteRunnableThread = new Thread(runnableThread);
+
+        infiniteRunnableThread.setDaemon(true);
+
+        counterThread.start();
+        infiniteRunnableThread.start();
     }
 }
